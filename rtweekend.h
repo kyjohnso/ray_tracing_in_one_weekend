96e8b9cf6340e1949117e611c943057cf309226e
kyle <kyjohnso@gmail.com>
2022-03-06 12:21:08 +0000

    add hittable object - against my better judgement

---
+++ rtweekend.h
@@ -0,0 +1,34 @@
+#ifndef RTWEEKEND_H
+#define RTWEEKEND_H
+
+#include <cmath>
+#include <limits>
+#include <memory>
+
+// Usings
+
+using std::shared_ptr;
+using std::make_shared;
+using std::sqrt;
+
+// Constants
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+// Utility Functions
+
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180.0;
+}
+
+inline double radians_to_degrees(double radians) {
+    return radians * 180 / pi;
+}
+
+// Common Headers
+
+#include "ray.h"
+#include "vec3.h"
+
+#endif
