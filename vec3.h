2c6e521eae7d4cf9b6b4aa83cf448683fe5f901b
kyle <kyjohnso@gmail.com>
2022-03-06 19:16:17 +0000

    start tinkering with materials

--- vec3.h
+++ vec3.h
@@ -11,6 +11,7 @@ class vec3 {
         vec3() : e{0,0,0} {}
         vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
 
+
         double x() const { return e[0]; }
         double y() const { return e[1]; }
         double z() const { return e[2]; }
@@ -43,6 +44,18 @@ class vec3 {
 
         double length_squared() const {
             return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+        }
+
+        inline static vec3 random() {
+            return vec3(random_double(), random_double(), random_double());
+        }
+        
+        inline static vec3 random(double min, double max) {
+            return vec3(
+                random_double(min,max),
+                random_double(min,max),
+                random_double(min,max)
+            );
         }
         
     public:
@@ -102,6 +115,15 @@ inline vec3 cross(const vec3 &u, const vec3 &v) {
 
 inline vec3 unit_vector(vec3 v) {
     return v/ v.length();
+}
+
+
+vec3 random_in_unit_sphere() {
+    while (true) {
+        auto p = vec3::random(-1,1);
+        if (p.length_squared() >= 1) continue;
+        return p;
+    }
 }
 
 #endif
