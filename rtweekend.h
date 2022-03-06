eb285f9be37869aa341ab8d64a4d8f9303168b05
kyle <kyjohnso@gmail.com>
2022-03-06 18:55:33 +0000

    add pixel averaging

--- rtweekend.h
+++ rtweekend.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <cstdlib>
 
 // Usings
 
@@ -24,6 +25,22 @@ inline double degrees_to_radians(double degrees) {
 
 inline double radians_to_degrees(double radians) {
     return radians * 180 / pi;
+}
+
+inline double random_double() {
+    // Returns a random real in [0,1).
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max-min)*random_double();
+}
+
+inline double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
 }
 
 // Common Headers
